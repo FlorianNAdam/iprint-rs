68a0a4dfe957959e0ab1ef8f8b98635e36fdd0c9
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2023-08-31 14:22:35 +0200

    add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,107 @@
+//! # iprint-rs
+//!
+//! `iprint-rs` is a Rust utility library for indented printing and logging,
+//! designed to help you easily trace the execution of your code. The library
+//! provides a suite of macros for indented formatting (`iformat`), simplified
+//! indented printing (`iprintln`), and logging with varying levels
+//! (`itrace`, `idebug`, `iinfo`, `iwarn`, `ierror`). All features are geared
+//! to improve debugging and code tracing by automatically adjusting indentation
+//! based on the function call depth.
+//!
+//! ## ⚠️ Warning
+//!
+//! This library may not function correctly when compiled in release mode due
+//! to function inlining. It is recommended to use it in debug mode for accurate results.
+//!
+//! ## Features
+//!
+//! - **iprintln! macro**: This is an enhanced version of `println!`, adding automatic indentation.
+//! - **iformat! macro**: Allows for custom indented formatting.
+//! - **call_depth! macro**: Provides the current depth of the function call stack,
+//!   useful for custom logging or tracing solutions.
+//! - **indented logging**: Offers five levels of logging (`itrace`, `idebug`, `iinfo`,
+//!   `iwarn`, `ierror`) that are feature-gated by the `log` feature.
+//!
+//! ## Installation
+//!
+//! To use the library, include it in your `Cargo.toml` like so:
+//!
+//! ```toml
+//! [dependencies]
+//! iprint-rs = "0.1.0"  # Use the latest version
+//! ```
+//!
+//! To enable the `log` feature for additional logging functionalities:
+//!
+//! ```toml
+//! [dependencies]
+//! iprint-rs = { version = "0.1.0", features = ["log"] }
+//! ```
+//!
+//! ## Usage Examples
+//!
+//! ### iprintln! macro
+//!
+//! ```rust
+//! use iprint::iprintln;
+//!
+//! fn my_function() {
+//!     iprintln!("This is like println! but with automatic indentation.");
+//!     another_function();
+//!     iprintln!("This will be indented like the first iprintln!");
+//! }
+//!
+//! fn another_function() {
+//!     iprintln!("This message will be more indented.");
+//! }
+//! ```
+//!
+//! ### iformat! macro
+//!
+//! ```rust
+//! use iprint::iformat;
+//!
+//! fn my_function() {
+//!     let msg = iformat!("This will be indented based on call depth.");
+//!     println!("{}", msg);
+//! }
+//! ```
+//!
+//! ### call_depth! macro
+//!
+//! ```rust
+//! use iprint::call_depth;
+//!
+//! fn custom_logging() {
+//!     let depth = call_depth!();
+//!     println!("Current call depth: {}", depth);
+//! }
+//! ```
+//!
+//! ### Logging Functions (Feature-Gated)
+//!
+//! To use logging functions, make sure you have the `log` feature enabled.
+//!
+//! ```rust
+//! use iprint::iinfo;
+//!
+//! fn yet_another_function() {
+//!     iinfo!("This is an informational message with automatic indentation.");
+//! }
+//! ```
+
+
 use std::cell::RefCell;
 
 #[cfg(feature="log")]
 pub use log::{trace, debug, info, warn, error};
 
-thread_local!(pub static STACK: RefCell<Vec<usize>> = RefCell::new(vec![]));
+thread_local!(
+    #[doc(hidden)]
+    pub static STACK: RefCell<Vec<usize>> = RefCell::new(vec![])
+);
 
+#[doc(hidden)]
 #[macro_export]
 macro_rules! stack_ptr {
     () => ({
@@ -16,6 +113,21 @@ macro_rules! stack_ptr {
     })
 }
 
+/// Retrieves the current call depth of the function stack.
+///
+/// This macro returns an integer representing the depth of the function
+/// call stack at the point where it is invoked.
+///
+/// # Example
+///
+/// ```
+/// use iprint::call_depth;
+///
+/// fn custom_logging() {
+///     let depth = call_depth!();
+///     println!("Current call depth: {}", depth);
+/// }
+/// ```
 #[macro_export]
 macro_rules! call_depth {
     () => ({
@@ -37,6 +149,22 @@ macro_rules! call_depth {
     })
 }
 
+/// Formats a given string with indentation based on the current call depth.
+///
+/// This macro works similarly to Rust's built-in `format!` macro,
+/// but prepends an indentation to the formatted string. The level of
+/// indentation is determined by the current call depth in the stack.
+///
+/// # Example
+///
+/// ```
+/// use iprint::iformat;
+///
+/// fn my_function() {
+///     let msg = iformat!("This will be indented based on call depth.");
+///     println!("{}", msg);
+/// }
+/// ```
 #[macro_export]
 macro_rules! iformat {
     ($($t:tt)*) => {{
@@ -52,6 +180,20 @@ macro_rules! iformat {
     }}
 }
 
+/// Prints a given string with automatic indentation to the console.
+///
+/// This macro is an enhanced version of Rust's `println!` macro,
+/// adding automatic indentation based on the current call depth.
+///
+/// # Example
+///
+/// ```
+/// use iprint::iprintln;
+///
+/// fn another_function() {
+///     iprintln!("This is like println! but with automatic indentation.");
+/// }
+/// ```
 #[macro_export]
 macro_rules! iprintln {
     ($($t:tt)*) => {
@@ -61,6 +203,23 @@ macro_rules! iprintln {
 
 #[cfg(feature="log")]
 pub mod ilog {
+    /// Logs a message at various levels with automatic indentation.
+    ///
+    /// These macros are feature-gated and can be enabled by activating the "log" feature.
+    /// They work similarly to their non-indented counterparts in the `log` crate but add
+    /// automatic indentation based on the call depth.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use iprint::iinfo;
+    ///
+    /// fn yet_another_function() {
+    ///     iinfo!("This is an informational message with automatic indentation.");
+    /// }
+    /// ```
+    ///
+    /// This macro is available only if the "log" feature is enabled.
     #[macro_export]
     macro_rules! itrace {
         ($($t:tt)*) => {
@@ -68,6 +227,22 @@ pub mod ilog {
         }
     }
 
+    /// Logs a debug message with automatic indentation.
+    ///
+    /// This macro is an enhanced version of the `debug!` macro from the `log` crate,
+    /// adding automatic indentation based on the current call depth.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use iprint::idebug;
+    ///
+    /// fn my_debug_function() {
+    ///     idebug!("This is a debug message with automatic indentation.");
+    /// }
+    /// ```
+    ///
+    /// This macro is available only if the "log" feature is enabled.
     #[macro_export]
     macro_rules! idebug {
         ($($t:tt)*) => {
@@ -75,6 +250,22 @@ pub mod ilog {
         }
     }
 
+    /// Logs an informational message with automatic indentation.
+    ///
+    /// This macro is an enhanced version of the `info!` macro from the `log` crate,
+    /// adding automatic indentation based on the current call depth.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use iprint::iinfo;
+    ///
+    /// fn my_info_function() {
+    ///     iinfo!("This is an informational message with automatic indentation.");
+    /// }
+    /// ```
+    ///
+    /// This macro is available only if the "log" feature is enabled.
     #[macro_export]
     macro_rules! iinfo {
         ($($t:tt)*) => {
@@ -82,6 +273,22 @@ pub mod ilog {
         }
     }
 
+    /// Logs a warning message with automatic indentation.
+    ///
+    /// This macro is an enhanced version of the `warn!` macro from the `log` crate,
+    /// adding automatic indentation based on the current call depth.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use iprint::iwarn;
+    ///
+    /// fn my_warn_function() {
+    ///     iwarn!("This is a warning message with automatic indentation.");
+    /// }
+    /// ```
+    ///
+    /// This macro is available only if the "log" feature is enabled.
     #[macro_export]
     macro_rules! iwarn {
         ($($t:tt)*) => {
@@ -89,6 +296,22 @@ pub mod ilog {
         }
     }
 
+    /// Logs an error message with automatic indentation.
+    ///
+    /// This macro is an enhanced version of the `error!` macro from the `log` crate,
+    /// adding automatic indentation based on the current call depth.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use iprint::ierror;
+    ///
+    /// fn my_error_function() {
+    ///     ierror!("This is an error message with automatic indentation.");
+    /// }
+    /// ```
+    ///
+    /// This macro is available only if the "log" feature is enabled.
     #[macro_export]
     macro_rules! ierror {
         ($($t:tt)*) => {
